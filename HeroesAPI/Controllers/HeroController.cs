9210acca57f9446918514557801bbd8b145ad66b
Nikos S <niksal1991@gmail.com>
2022-02-12 08:21:01 +0200

    Crud WebApi

--- HeroesAPI/Controllers/HeroController.cs
+++ HeroesAPI/Controllers/HeroController.cs
@@ -17,6 +17,15 @@ namespace HeroesAPI.Controllers
                 FirstName = "Peter",
                 LastName = "Parker",
                 Place = "New York"
+            },
+
+            new Hero
+            {
+                Id = 2,
+                Name = "Ironman",
+                FirstName = "Tony",
+                LastName = "Stark",
+                Place = "Las Vegas"
             }
         };
 
@@ -35,12 +44,66 @@ namespace HeroesAPI.Controllers
             }
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Hero>>> GetOneHero(int id)
+        {
+            try
+            {
+                var hero = heroes.FirstOrDefault(h => h.Id == id);
+
+                if (hero is null)
+                {
+                    return BadRequest("Hero not found");
+                }
+
+                return Ok(hero);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
 
         [HttpPost]
         public async Task<ActionResult<List<Hero>>> AddHero(Hero newHero)
         {
             heroes.Add(newHero);
             return Ok(newHero);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Hero>> UpdateHero(Hero requestedHero)
+        {
+            var hero = heroes.FirstOrDefault(h => h.Id == requestedHero.Id);
+
+            if (hero is null)
+            {
+                return BadRequest("Hero not found");
+            }
+
+            hero.Name = requestedHero.Name;
+            hero.FirstName = requestedHero.FirstName;
+            hero.LastName = requestedHero.LastName;
+            hero.Place = requestedHero.Place;
+
+            return Ok(hero);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteHero(int id)
+        {
+            var hero = heroes.FirstOrDefault(h => h.Id == requestedHero.Id);
+
+            if (hero is null)
+            {
+                return BadRequest("Hero not found");
+            }
+
+            heroes.Remove(hero);
+
+            return Ok();
+
         }
 
     }
