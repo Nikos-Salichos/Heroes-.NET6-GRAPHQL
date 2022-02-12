a4dab91a5492e2b85e32a8c4f72da91df1e4b79b
Nikos S <niksal1991@gmail.com>
2022-02-12 07:54:41 +0200

    Add project files.

---
+++ HeroesAPI/Controllers/HeroController.cs
@@ -0,0 +1,47 @@
+﻿using HeroesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeroesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroController : ControllerBase
+    {
+
+        private static List<Hero> heroes = new List<Hero>
+        {
+            new Hero
+            {
+                Id = 1,
+                Name = "Spider Man",
+                FirstName = "Peter",
+                LastName = "Parker",
+                Place = "New York"
+            }
+        };
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Hero>>> GetAllHeroes()
+        {
+            try
+            {
+                return Ok(heroes);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<List<Hero>>> AddHero(Hero newHero)
+        {
+            heroes.Add(newHero);
+            return Ok(newHero);
+        }
+
+    }
+}
