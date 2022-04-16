ce19a87fa46b2062570e6e7c41b3e9b9a44783ee
Nikos S <niksal1991@gmail.com>
2022-04-16 09:58:55 +0300

    Implemented graphQL integration tools

---
+++ HeroesAPI/GraphQL/HeroType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+using HeroesAPI.Entities.Models;
+
+namespace HeroesAPI.GraphQL
+{
+    public class HeroType : ObjectGraphType<Hero>
+    {
+        public HeroType()
+        {
+            Field(h => h.Id);
+            Field(h => h.Name);
+            Field(h => h.Place);
+        }
+    }
+}
