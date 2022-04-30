6f4c97e06ba03a393b560ae08a9f98eb54c82411
Nikos S <niksal1991@gmail.com>
2022-04-30 20:21:44 +0300

    Implemented FileController and GetFile

--- HeroesAPI/Controllers/FilesController.cs
+++ HeroesAPI/Controllers/FilesController.cs
@@ -6,6 +6,19 @@ namespace HeroesAPI.Controllers
     [ApiController]
     public class FilesController : ControllerBase
     {
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string field)
+        {
+            var pathToFile = "";
 
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, "text/plain", Path.GetFileName(pathToFile));
+
+        }
     }
 }
