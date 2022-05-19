142c96091235d98b577610a13951c51cf0e820e6
Nikos S <niksal1991@gmail.com>
2022-05-19 21:58:23 +0300

    Implemented IHeroRepository to return Task

--- HeroesAPI/Interfaces/IHeroRepository.cs
+++ HeroesAPI/Interfaces/IHeroRepository.cs
@@ -7,7 +7,7 @@ namespace HeroesAPI.Interfaces
         Task<List<Hero>> GetAllHeroesAsync();
         Task<Hero?> GetHeroByIdAsync(int heroId);
         Task<Hero?> CreateHero(Hero hero);
-        Hero UpdateHero(Hero hero);
+        Task<Hero?> UpdateHero(Hero hero);
         void DeleteHero(Hero hero);
         string CreateImageDirectory();
         void SaveImageInDir(Hero newHero, string pathToSave, out string fullPath, out string extension);
