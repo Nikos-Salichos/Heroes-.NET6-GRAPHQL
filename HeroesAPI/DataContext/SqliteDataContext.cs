df7abfb65c557b1c0ff7d3fd6e7319c4901809a6
Nikos S <niksal1991@gmail.com>
2022-05-21 00:54:06 +0300

    Hide ConnectionString

--- HeroesAPI/DataContext/SqliteDataContext.cs
+++ HeroesAPI/DataContext/SqliteDataContext.cs
@@ -1,11 +1,32 @@
-﻿namespace HeroesAPI.DataContext
+﻿using HeroesAPI.Models;
+
+namespace HeroesAPI.DataContext
 {
     public class SqliteDataContext : DbContext
     {
-        public SqliteDataContext(DbContextOptions<MainDbContextInfo> options) : base(options)
+        protected readonly IConfiguration Configuration;
+
+        public SqliteDataContext(DbContextOptions<SqliteDataContext> options, IConfiguration configuration) : base(options)
         {
+            Configuration = configuration;
         }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // connect to sql server with connection string from app settings
+            optionsBuilder.UseSqlite(Configuration.GetConnectionString("SqliteConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Hero>().HasData(
+                 new Hero { Id = 9, Name = "Thor", FirstName = "Thor", LastName = "Odinson", Place = "Asgard" }
+                 );
+        }
+
+        public DbSet<Hero> Heroes { get; set; }
 
     }
 }
---
+++ HeroesAPI/Migrations/SqliteData/20220520195037_HeroDBSQLite.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HeroesAPI.Migrations.SqliteData
+{
+    public partial class HeroDBSQLite : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Heroes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Place = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    ImageUrl = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Heroes", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Heroes",
+                columns: new[] { "Id", "FirstName", "ImageUrl", "LastName", "Name", "Place" },
+                values: new object[] { 9, "Thor", null, "Odinson", "Thor", "Asgard" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Heroes");
+        }
+    }
+}
